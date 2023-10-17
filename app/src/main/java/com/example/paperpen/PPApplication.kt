09bac12a3d5a9912d2ecec36afaaac4e2ecbf389
ayush <ayush.gupta43.ag@gmail.com>
2023-10-18 00:37:40 +0530

    Commit

---
+++ app/src/main/java/com/example/paperpen/PPApplication.kt
@@ -0,0 +1,9 @@
+package com.example.paperpen
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlin.text.Typography.dagger
+
+@HiltAndroidApp
+class PPApplication : Application() {
+}
---
+++ app/src/main/java/com/example/paperpen/util/Utils.kt
@@ -0,0 +1,4 @@
+package com.example.paperpen.util
+
+val <T> T.exhaustive: T
+    get() = this
